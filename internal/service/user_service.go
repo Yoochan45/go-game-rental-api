690fd273c74ff9c3f5760f9653d1a15ac48dd8cd
Aisiya Qutwatunnada <ycorp00@gmail.com>
2025-11-05 14:51:41 +0700

    refactor: remove unused user service methods for cleaner interface

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -23,9 +23,6 @@ type UserService interface {
 	Register(registerData interface{}) (*model.User, error)
 	Login(loginData interface{}, jwtSecret string) (interface{}, error)
 	RefreshToken(refreshData interface{}, jwtSecret string) (interface{}, error)
-	CreateUser(userData *model.User) error
-	AuthenticateUser(email, password string) (*model.User, error)
-	GetUserByID(userID uint) (*model.User, error)
 	ChangePassword(userID uint, currentPassword, newPassword string) error
 
 	// Admin methods
@@ -33,14 +30,9 @@ type UserService interface {
 	GetUserDetail(requestorRole model.UserRole, userID uint) (*model.User, error)
 	UpdateUserRole(requestorRole model.UserRole, userID uint, newRole model.UserRole) error
 	ToggleUserStatus(requestorRole model.UserRole, userID uint) error
-	BanUser(requestorID uint, requestorRole model.UserRole, targetUserID uint) error
-	UnbanUser(requestorID uint, requestorRole model.UserRole, targetUserID uint) error
 
 	// Super Admin methods
-	CreateAdmin(requestorRole model.UserRole, userData *model.User) error
 	DeleteUser(requestorID uint, requestorRole model.UserRole, targetUserID uint) error
-	PromoteToAdmin(requestorRole model.UserRole, userID uint) error
-	DemoteFromAdmin(requestorRole model.UserRole, userID uint) error
 }
 
 type userService struct {
@@ -58,26 +50,6 @@ func (s *userService) GetProfile(userID uint) (*model.User, error) {
 func (s *userService) UpdateProfile(userID uint, updateData interface{}) error {
 	// TODO: Implement proper DTO to model conversion
 	return errors.New("not implemented yet")
-}
-
-func (s *userService) CreateUser(userData *model.User) error {
-	return s.userRepo.Create(userData)
-}
-
-func (s *userService) AuthenticateUser(email, password string) (*model.User, error) {
-	user, err := s.userRepo.GetByEmail(email)
-	if err != nil {
-		return nil, errors.New("invalid credentials")
-	}
-
-	// Add password verification logic here
-	// This should use bcrypt or similar to verify password
-
-	return user, nil
-}
-
-func (s *userService) GetUserByID(userID uint) (*model.User, error) {
-	return s.userRepo.GetByID(userID)
 }
 
 func (s *userService) Register(registerData interface{}) (*model.User, error) {
@@ -136,45 +108,6 @@ func (s *userService) ToggleUserStatus(requestorRole model.UserRole, userID uint
 	return s.userRepo.UpdateActiveStatus(userID, !user.IsActive)
 }
 
-func (s *userService) BanUser(requestorID uint, requestorRole model.UserRole, targetUserID uint) error {
-	if !s.canManageUsers(requestorRole) {
-		return ErrInsufficientPermission
-	}
-
-	if requestorID == targetUserID {
-		return ErrCannotDeleteSelf
-	}
-
-	targetUser, err := s.userRepo.GetByID(targetUserID)
-	if err != nil {
-		return ErrUserNotFound
-	}
-
-	// Admin can't ban other admins or super admins
-	if requestorRole == model.RoleAdmin && (targetUser.Role == model.RoleSuperAdmin || targetUser.Role == model.RoleAdmin) {
-		return ErrInsufficientPermission
-	}
-
-	return s.userRepo.UpdateActiveStatus(targetUserID, false)
-}
-
-func (s *userService) UnbanUser(requestorID uint, requestorRole model.UserRole, targetUserID uint) error {
-	if !s.canManageUsers(requestorRole) {
-		return ErrInsufficientPermission
-	}
-
-	return s.userRepo.UpdateActiveStatus(targetUserID, true)
-}
-
-func (s *userService) CreateAdmin(requestorRole model.UserRole, userData *model.User) error {
-	if requestorRole != model.RoleSuperAdmin {
-		return ErrInsufficientPermission
-	}
-
-	userData.Role = model.RoleAdmin
-	return s.userRepo.Create(userData)
-}
-
 func (s *userService) DeleteUser(requestorID uint, requestorRole model.UserRole, targetUserID uint) error {
 	if requestorRole != model.RoleSuperAdmin {
 		return ErrInsufficientPermission
@@ -195,22 +128,6 @@ func (s *userService) DeleteUser(requestorID uint, requestorRole model.UserRole,
 	}
 
 	return s.userRepo.Delete(targetUserID)
-}
-
-func (s *userService) PromoteToAdmin(requestorRole model.UserRole, userID uint) error {
-	if requestorRole != model.RoleSuperAdmin {
-		return ErrInsufficientPermission
-	}
-
-	return s.userRepo.UpdateRole(userID, model.RoleAdmin)
-}
-
-func (s *userService) DemoteFromAdmin(requestorRole model.UserRole, userID uint) error {
-	if requestorRole != model.RoleSuperAdmin {
-		return ErrInsufficientPermission
-	}
-
-	return s.userRepo.UpdateRole(userID, model.RoleCustomer)
 }
 
 // Helper methods
